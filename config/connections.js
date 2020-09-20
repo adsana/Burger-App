95d539b7ee350bdd4f431561cd14f8b954dae71d
Adsana <adsanasayasith@gmail.com>
2020-09-20 05:01:31 -0400

    updated connections for deployment

--- config/connections.js
+++ config/connections.js
@@ -6,9 +6,9 @@ if (process.env.JAWSDB_URL) {
 } else {
   connection = mysql.createConnection({
     port: 3306,
-    host: "localhost",
-    user: "root",
-    password: "",
+    host: "d1kb8x1fu8rhcnej.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "kgqhjs83oh0b497t",
+    password: "rbcfx21jep2lxojq",
     database: "burgers_db"
   });
 };
